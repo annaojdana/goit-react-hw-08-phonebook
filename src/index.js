aa4db01b4c0e62707cd1676b72c45e5ac3486c56
Anna Ojdana <anna.ojdana@gmail.com>
2022-10-11 11:39:54 +0200

    adding routes

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import './index.css';
+
+
 import { Provider } from 'react-redux';
 import store from 'redux/store';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
