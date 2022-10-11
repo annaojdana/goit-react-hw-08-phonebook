de4fae049382322a6a9b3cfc159ba5af4630a9ab
Anna Ojdana <anna.ojdana@gmail.com>
2022-10-11 13:05:22 +0200

    creating a publicLayout

---
+++ src/components/PublicLayout/PublicLayout.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+
+const PublicLayout = () => {
+  const content = (
+    <section className="public">
+      <header>
+        <h1>Welcome to Phonebook!</h1>
+      </header>
+      <main>
+        <p>
+          Located in Beautiful Downtown Foo City, Phonebook provides a
+          trained staff ready to meet your repair needs.
+        </p>
+        <p>&nbsp;</p>
+        <address>
+          Phonebook
+          <br />
+          555 Foo Drive
+          <br />
+          Foo City, CA 12345
+          <br />
+          <a href="tel:+15555555555">(555) 555-5555</a>
+        </address>
+      </main>
+      <footer>
+        <Link to="/login">Login</Link>
+      </footer>
+    </section>
+  );
+  return content;
+};
+export default PublicLayout;
