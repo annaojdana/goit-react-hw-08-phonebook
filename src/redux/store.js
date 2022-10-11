7f8fc9f16cfe86f246fd7f3e1f0ce0756a14a41e
Anna Ojdana <anna.ojdana@gmail.com>
2022-10-11 12:55:43 +0200

    create store with baseApi reducer and auth reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsApi } from 'services/contactsApi';
 import filterReducer from './slices/filterSlice';
+import authReducer from './slices/auth/authSlice'
+import { baseApi } from 'services/baseApi';
 
 const store = configureStore({
   reducer: {
     filter: filterReducer,
     [contactsApi.reducerPath]: contactsApi.reducer,
+    [baseApi.reducerPath]: baseApi.reducer,
+    auth: authReducer,
   },
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(contactsApi.middleware),
+    getDefaultMiddleware().concat(contactsApi.middleware, baseApi.middleware),
   devTools: true,
 });
 
