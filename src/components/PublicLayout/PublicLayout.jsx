b7c62028f96ef09247d5666ccce906eb8f78cf93
Anna Ojdana <anna.ojdana@gmail.com>
2022-10-12 19:33:35 +0200

    install react-icons

--- src/components/PublicLayout/PublicLayout.jsx
+++ src/components/PublicLayout/PublicLayout.jsx
@@ -1,29 +1,42 @@
 import { Link } from 'react-router-dom';
+import styles from './PublicLayout.module.css';
+import myPhoto from './anna_ojdana_profile.jpg';
+import { IoLogoLinkedin, IoLogoGithub } from 'react-icons/io';
 
 const PublicLayout = () => {
+  const { heading, description, presentation, photo } = styles;
+
   const content = (
     <section className="public">
-      <header>
-        <h1>Welcome to Phonebook!</h1>
-      </header>
-      <main>
-        <p>
-          Located in Beautiful Downtown Foo City, Phonebook provides a
-          trained staff ready to meet your repair needs.
-        </p>
-        <p>&nbsp;</p>
-        <address>
-          Phonebook
-          <br />
-          555 Foo Drive
-          <br />
-          Foo City, CA 12345
-          <br />
-          <a href="tel:+15555555555">(555) 555-5555</a>
-        </address>
-      </main>
-      <footer>
-      </footer>
+      <h1 className={heading}>Welcome to my Phonebook App</h1>
+      <p className={description}>
+        This app was made during my learning react and redux. It allows you to
+        register and keep your contacts.
+      </p>
+
+      <div className={presentation}>
+        <div>
+          <img
+            className={photo}
+            src={myPhoto}
+            alt="profile photo of the creator of the application"
+          />
+        </div>
+        <div>
+          <p className={description}>
+            Hi, my name is Ania and I'm a Junior React Developer
+          </p>
+          <p> Contact: </p>
+          <a href="https://www.linkedin.com/in/anna-ojdana/">
+            <IoLogoLinkedin />
+          </a>
+          <a href="https://github.com/annaojdana/goit-react-hw-08-phonebook">
+            <IoLogoGithub />
+          </a>
+        </div>
+      </div>
+
+      <footer></footer>
     </section>
   );
   return content;
