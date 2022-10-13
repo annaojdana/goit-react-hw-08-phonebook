a5c48d94102460a5ff79f66983d5079b8b12db52
Anna Ojdana <anna.ojdana@gmail.com>
2022-10-13 14:43:01 +0200

    redux-persist

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from 'components/App';
 import './index.css';
 
 
 import { Provider } from 'react-redux';
-import store from 'redux/store';
+import { store, persistor } from 'redux/store';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,17 +3,40 @@ import { contactsApi } from 'services/contactsApi';
 import filterReducer from './slices/filterSlice';
 import authReducer from './slices/auth/authSlice'
 import { baseApi } from 'services/baseApi';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
-const store = configureStore({
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+export const store = configureStore({
   reducer: {
     filter: filterReducer,
     [contactsApi.reducerPath]: contactsApi.reducer,
     [baseApi.reducerPath]: baseApi.reducer,
     auth: authReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(contactsApi.middleware, baseApi.middleware),
+  middleware,
   devTools: true,
 });
+export const persistor = persistStore(store);
 
-export default store;
