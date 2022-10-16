cbfbb85de4597b8b90cde96144c21479141c190c
Anna Ojdana <anna.ojdana@gmail.com>
2022-10-16 11:06:14 +0200

    adding BrowserRoute

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { PersistGate } from 'redux-persist/integration/react';
-import App from 'components/App';
-import './index.css';
-
-
 import { Provider } from 'react-redux';
 import { store, persistor } from 'redux/store';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from 'components/App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <Routes>
-            <Route path="/*" element={<App />} />
-          </Routes>
+          <App/>
         </BrowserRouter>
       </PersistGate>
     </Provider>
